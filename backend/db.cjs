64ad651b335e313c41867f0815a8e30ca18e2439
whoisgitesh <giteshpal25.04.03@gmail.com>
2026-06-03 14:56:54 +0530

    add env varibale

--- backend/db.cjs
+++ backend/db.cjs
@@ -1,11 +1,22 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "Punjab@100",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "Punjab@100",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = "your_secret_key";
+const JWT_SECRET = "e19db21d39525c5ea560c46d06e7053cbd7055daa7e72d3d713eb726e0dde92e2f5ac1fb013e9e1870d05ca7ab7989eea7427855f250e3747bbd4a91bb11ac76";
 
 export const generateToken = (user) => {
   return jwt.sign(
